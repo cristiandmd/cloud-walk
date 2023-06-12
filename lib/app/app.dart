59b182e0eb5ab4411eba6068e78f1e1261e5624a
Cristian Madrid <cristianmadridd@outlook.com>
2023-06-12 20:44:14 -0300

    chore: add basic localization structure

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import '../generated/l10n.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello World'),
+    return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: const Text('Hello World'),
     );
   }
 }
