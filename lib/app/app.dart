c0f053b2385c88aa7d86735ab81ec4d79f4de97f
Cristian Madrid <cristianmadridd@outlook.com>
2023-06-13 16:15:46 -0300

    inject routing system at main

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import '../generated/l10n.dart';
+import 'routes/build_routes.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -14,6 +15,7 @@ class App extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
+      onGenerateRoute: buildRoutes(appRoutes),
       supportedLocales: S.delegate.supportedLocales,
       home: const Scaffold(body: Center(child: Text('Hello World'))),
     );
