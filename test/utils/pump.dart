0a0215c9506f5d5e19508dc8b3ac0e7aed984873
Cristian Madrid <cristianmadridd@outlook.com>
2023-06-13 16:07:09 -0300

    test: create shared test utils functions

---
+++ test/utils/pump.dart
@@ -0,0 +1,35 @@
+import 'package:concerts_weather/app/app_routes.dart';
+import 'package:concerts_weather/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+const _navigatorKey = Key('navigator');
+
+extension Tester on WidgetTester {
+  Future<void> pumpSUT({
+    required Iterable<AppRoute> routes,
+    NavigatorObserver? observer,
+  }) async {
+    await pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        navigatorObservers: [if (observer != null) observer],
+        onGenerateRoute: buildRoutes(routes),
+        home: const SizedBox(key: _navigatorKey),
+      ),
+    );
+    await pumpAndSettle();
+  }
+
+  void navigateTo(String routeName) {
+    final context = element(find.byKey(_navigatorKey));
+    Navigator.of(context).pushNamed(routeName);
+  }
+}
