bcb7cb02840b1b4a793711f89e9ab909a864b1f5
Cristian Madrid <cristianmadridd@outlook.com>
2023-06-14 13:48:40 -0300

    feat: add http and equatable library

--- lib/modules/current_weather/domain/repositories/weather_model.dart
+++ lib/modules/current_weather/domain/repositories/weather_model.dart
@@ -1,3 +1,10 @@
-class WeatherModel {
+import 'package:equatable/equatable.dart';
+
+class WeatherModel extends Equatable {
+  const WeatherModel.fromJson(Map<String, dynamic> json);
+  Map<String, dynamic> toJson() => {};
   const WeatherModel();
+
+  @override
+  List<Object?> get props => [];
 }
--- lib/modules/forecast/domain/repositories/forecast_model.dart
+++ lib/modules/forecast/domain/repositories/forecast_model.dart
@@ -1,5 +1,12 @@
-class ForecastModel {
+import 'package:equatable/equatable.dart';
+
+class ForecastModel extends Equatable {
   const ForecastModel();
+  const ForecastModel.fromJson(Map<String, dynamic> json);
+  Map<String, dynamic> toJson() => {};
+
+  @override
+  List<Object?> get props => [];
 }
 
 enum ForecastRange { nextFiveDays }
